f4002e3c4b1e86b30953a0509cb25c1d015e8b4c
lhadley91 <52940778+lhadley91@users.noreply.github.com>
2024-07-13 14:26:44 +0200

    Update unemploymentDataProcessor.R to see the data

--- unemploymentDataProcessor.R
+++ unemploymentDataProcessor.R
@@ -1305,9 +1305,13 @@ labor_force_info <- bind_rows(
   map_dfr(c("CLF16OV", "DCLF", paste0(state.abb, "LF")), get_fred_series_with_state_id, "labor_force_sa", sleep = TRUE),
   map_dfr(c("CIVPART", paste0("LBSSA", str_pad(1:56, width = 2, side = "left", pad = "0"))), get_fred_series_with_state_id, "labor_force_participation_rate_sa", sleep = TRUE)
 ) %>% 
-  pivot_wider(names_from = metric, values_from = value) %>% 
-  # Ensure columns are numeric
-  mutate(across(c(labor_force_sa, labor_force_participation_rate_sa), as.numeric)) %>%
+  pivot_wider(names_from = metric, values_from = value) %>%
+  # Print the values of labor_force_sa and labor_force_participation_rate_sa
+  mutate(across(c(labor_force_sa, labor_force_participation_rate_sa), ~{
+    message("labor_force_sa: ", .data$labor_force_sa)
+    message("labor_force_participation_rate_sa: ", .data$labor_force_participation_rate_sa)
+    .
+  })) %>%
   # Calculate the new column
   mutate(civilian_non_insitutionalized_population_sa = 100 * labor_force_sa / labor_force_participation_rate_sa) %>%
   pivot_longer(cols = 3:5, names_to = "metric")
