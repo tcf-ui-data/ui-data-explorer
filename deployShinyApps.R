8428a5c6c514e7990fb8e2ecefb8291de63df97e
TCF UI Project <68959901+tcf-ui-data@users.noreply.github.com>
2025-11-10 09:45:23 -0500

    Update deployShinyApps.R

--- deployShinyApps.R
+++ deployShinyApps.R
@@ -4,6 +4,9 @@
 # Deploy this app to shinyapps.io.
 
 message("Deploying to shinyapps.")
+cat("Account name:", Sys.getenv("SHINYAPPS_ACCOUNT"), "\n")
+cat("Token present:", nchar(Sys.getenv("SHINYAPPS_TOKEN")) > 0, "\n")
+cat("Secret present:", nchar(Sys.getenv("SHINYAPPS_SECRET")) > 0, "\n")
 rsconnect::setAccountInfo(name=Sys.getenv("SHINYAPPS_ACCOUNT"), token=Sys.getenv("SHINYAPPS_TOKEN"), secret=Sys.getenv("SHINYAPPS_SECRET"))
 rsconnect::deployApp(config::get("PROJECT_ROOT"),
                      appName="ui-data-explorer",
